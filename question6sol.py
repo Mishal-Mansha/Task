0fa7104cd7ad7071cb1b58207d78124e1378b08b
Mishal-Mansha <52660386+Mishal-Mansha@users.noreply.github.com>
2019-07-26 19:12:44 +0500

    Update question6sol.py

--- question6sol.py
+++ question6sol.py
@@ -1,7 +1,1 @@
-string=input("Enter a list of numbers:")
-lists=string.split(',')
-minNum=int(lists[0])
-for i in lists:
-    if(int(i)<minNum):
-        minNum=int(i)
-print("Minimum number in the list is:",minNum)
+
