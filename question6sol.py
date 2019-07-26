7e8567614865a074fc3bffe16beaba6612c6a6be
Mishal-Mansha <52660386+Mishal-Mansha@users.noreply.github.com>
2019-07-26 21:48:50 +0500

    Update question6sol.py

--- question6sol.py
+++ question6sol.py
@@ -1,1 +1,11 @@
 
+string=input("Enter a comma seperated list of numbers:")
+if(len(string)>=2):
+    lists=string.split(",")
+    list1=[]
+    for i in lists:
+        list1.append(int(i))
+    list1.sort()
+    print("Second minimum number is:",list1[1])
+else:
+    print("For second minimum numbber enter more than one number")
